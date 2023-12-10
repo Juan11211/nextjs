0f6dd4a2deeb516e40816e6c3e2ed8c1f977d3a0
Juan11211 <avilesjuan628@gmail.com>
2023-12-10 00:50:52 -0500

    working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import '@/app/ui/global.css'
-import { Inter } from 'next/font/google';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
  
-export const inter = Inter({ subsets: ['latin'] });
-
 export default function RootLayout({
   children,
 }: {
@@ -10,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-         <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
